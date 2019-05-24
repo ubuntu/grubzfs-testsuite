0c70f06dd140c60547507c16a0c488b7c8a854a8
Didier Roche <didrocks@ubuntu.com>
2019-05-24 13:08:32 +0200

    Fix grub-probe mock calling the real one
    In TestBootlist, we are calling the real grub-probe to match the
    underlying device.
    In TestGrubMkConfig, we are using the mock grub-probe for the whole stack,
    and thus, not matchin the underlying device.
    Now, fallback for the only call made by TestBootlist to the real
    grub-probe binary. This will be called again when generating the grub
    menu in the 3rd step, but will be a no-op.
    
    Thanks to this, we can reuse the same grubmenu reference files.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- cmd/grub-probe/main.go
+++ cmd/grub-probe/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
-
 	switch os.Args[1] {
 	case "--target=device":
-		fmt.Println("UUID-" + os.Args[2])
+		cmd := exec.Command("/usr/sbin/grub-probe", os.Args[1:]...)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		if err := cmd.Run(); err != nil {
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				os.Exit(exiterr.ExitCode())
+			}
+			fmt.Println("Unexpected error when trying to execute grube-prove", err)
+			os.Exit(2)
+		}
 		os.Exit(0)
 	case "--device":
 		if !strings.HasPrefix(os.Args[3], "--target") {
