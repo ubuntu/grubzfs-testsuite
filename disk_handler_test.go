7c2257018377c1b2770f7db1ce32c18c175b5898
Didier Roche <didrocks@ubuntu.com>
2019-06-11 18:00:01 +0200

    Ensure we close and export expected pools
    As those defer were ran in a for loop, only the last pool was properly
    closed and exported. Wrap this by a function.
    
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- disk_handler_test.go
+++ disk_handler_test.go
@@ -80,172 +80,173 @@ func (fdevice FakeDevices) create(path, testName string) string {
 	var systemRootDataset string
 
 	for _, device := range fdevice.Devices {
-
-		// Create file on disk
-		p := filepath.Join(path, device.Name+".disk")
-		f, err := os.Create(p)
-		if err != nil {
-			fdevice.Fatal("couldn't create device file on disk", err)
-		}
-		if err = f.Truncate(100 * mB); err != nil {
-			f.Close()
-			fdevice.Fatal("couldn't initializing device size on disk", err)
-		}
-		f.Close()
-
-		deviceMountPath := filepath.Join(path, device.Name)
-		if err := os.MkdirAll(deviceMountPath, 0700); err != nil {
-			fdevice.Fatal("couldn't create directory for pool", err)
-		}
-
-		switch strings.ToLower(device.Type) {
-		case "zfs":
-			// WORKAROUND: we need to use dirname of path as creating 2 consecutives pools with similar dataset name
-			// will make the second dataset from the second pool returning as parent pool the first one.
-			// Of course, the resulting mountpoint will be wrong.
-			poolName := testName + "-" + device.ZFS.PoolName
-
-			vdev := zfs.VDevTree{
-				Type:    zfs.VDevTypeFile,
-				Path:    p,
-				Devices: []zfs.VDevTree{{Type: zfs.VDevTypeFile, Path: p}},
-			}
-
-			features := make(map[string]string)
-			props := make(map[zfs.Prop]string)
-			props[zfs.PoolPropAltroot] = deviceMountPath
-			fsprops := make(map[zfs.Prop]string)
-			mountpoint := "/"
-			fsprops[zfs.DatasetPropCanmount] = "off"
-			if device.ZFS.MountPoint != "" {
-				mountpoint = device.ZFS.MountPoint
-				fsprops[zfs.DatasetPropCanmount] = "on"
-			}
-			fsprops[zfs.DatasetPropMountpoint] = mountpoint
-
-			pool, err := zfs.PoolCreate(poolName, vdev, features, props, fsprops)
+		func() {
+			// Create file on disk
+			p := filepath.Join(path, device.Name+".disk")
+			f, err := os.Create(p)
 			if err != nil {
-				fdevice.Fatalf("couldn't create pool %q: %v", device.ZFS.PoolName, err)
+				fdevice.Fatal("couldn't create device file on disk", err)
 			}
-			defer pool.Close()
-			defer func() {
-				if device.ZFS.KeepImported {
-					return
+			if err = f.Truncate(100 * mB); err != nil {
+				f.Close()
+				fdevice.Fatal("couldn't initializing device size on disk", err)
+			}
+			f.Close()
+
+			deviceMountPath := filepath.Join(path, device.Name)
+			if err := os.MkdirAll(deviceMountPath, 0700); err != nil {
+				fdevice.Fatal("couldn't create directory for pool", err)
+			}
+
+			switch strings.ToLower(device.Type) {
+			case "zfs":
+				// WORKAROUND: we need to use dirname of path as creating 2 consecutives pools with similar dataset name
+				// will make the second dataset from the second pool returning as parent pool the first one.
+				// Of course, the resulting mountpoint will be wrong.
+				poolName := testName + "-" + device.ZFS.PoolName
+
+				vdev := zfs.VDevTree{
+					Type:    zfs.VDevTypeFile,
+					Path:    p,
+					Devices: []zfs.VDevTree{{Type: zfs.VDevTypeFile, Path: p}},
 				}
-				pool.Export(true, "export temporary pool")
-			}()
 
-			for _, dataset := range device.ZFS.Datasets {
-				func() {
-					datasetName := poolName + "/" + dataset.Name
-					if dataset.IsCurrentSystemRoot {
-						systemRootDataset = datasetName
-					}
-					datasetPath := ""
-					shouldMount := false
-					props := make(map[zfs.Prop]zfs.Property)
-					if dataset.Mountpoint != "" {
-						props[zfs.DatasetPropMountpoint] = zfs.Property{Value: dataset.Mountpoint}
-					}
-					if dataset.CanMount != "" {
-						props[zfs.DatasetPropCanmount] = zfs.Property{Value: dataset.CanMount}
-						if dataset.CanMount == "noauto" || dataset.CanMount == "on" {
-							shouldMount = true
-						}
-					}
-					d, err := zfs.DatasetCreate(datasetName, zfs.DatasetTypeFilesystem, props)
-					if err != nil {
-						fdevice.Fatalf("couldn't create dataset %q: %v", datasetName, err)
-					}
-					defer d.Close()
-					if dataset.ZsysBootfs {
-						d.SetUserProperty("org.zsys:bootfs", "yes")
-						if !dataset.LastUsed.IsZero() {
-							d.SetUserProperty("org.zsys:last-used", strconv.FormatInt(dataset.LastUsed.Unix(), 10))
-						}
-					}
-					if dataset.LastBootedKernel != "" {
-						d.SetUserProperty("org.zsys:last-booted-kernel", dataset.LastBootedKernel)
-					}
-					if shouldMount {
-						// get potentially inherited mountpoint path
-						mountProp, err := d.GetProperty(zfs.DatasetPropMountpoint)
-						if err != nil {
-							fdevice.Fatalf("couldn't get mount point for %q: %v", datasetName, err)
-						}
-						datasetPath = mountProp.Value
-						if datasetPath != "legacy" {
-							if err := d.Mount("", 0); err != nil {
-								fdevice.Fatalf("couldn't mount dataset: %q: %v", datasetName, err)
-							}
-							// Mount manually datasetPath if set to legacy to "/" (deviceMountPath)
-						} else {
-							datasetPath = deviceMountPath
-							if err := syscall.Mount(datasetName, datasetPath, "zfs", 0, ""); err != nil {
-								fdevice.Fatalf("couldn't manually mount dataset: %q: %v", datasetName, err)
-							}
-						}
+				features := make(map[string]string)
+				props := make(map[zfs.Prop]string)
+				props[zfs.PoolPropAltroot] = deviceMountPath
+				fsprops := make(map[zfs.Prop]string)
+				mountpoint := "/"
+				fsprops[zfs.DatasetPropCanmount] = "off"
+				if device.ZFS.MountPoint != "" {
+					mountpoint = device.ZFS.MountPoint
+					fsprops[zfs.DatasetPropCanmount] = "on"
+				}
+				fsprops[zfs.DatasetPropMountpoint] = mountpoint
 
-						defer os.RemoveAll(datasetPath)
-						defer d.UnmountAll(0)
+				pool, err := zfs.PoolCreate(poolName, vdev, features, props, fsprops)
+				if err != nil {
+					fdevice.Fatalf("couldn't create pool %q: %v", device.ZFS.PoolName, err)
+				}
+				defer pool.Close()
+				defer func() {
+					if device.ZFS.KeepImported {
+						return
 					}
-
-					for _, s := range dataset.Snapshots {
-						func() {
-							replaceContent(fdevice.T, s.Content, datasetPath)
-							completeSystemWithFstab(fdevice.T, testName, path, dataset.Mountpoint, datasetPath, false, time.Time{}, s.Fstab)
-							props := make(map[zfs.Prop]zfs.Property)
-							d, err := zfs.DatasetSnapshot(datasetName+"@"+s.Name, false, props)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "Couldn't create snapshot %q: %v\n", datasetName+"@"+s.Name, err)
-								os.Exit(1)
-							}
-							defer d.Close()
-
-							// Convert time in current timezone for mock
-							location, err := time.LoadLocation("Local")
-							if err != nil {
-								fdevice.Fatal("couldn't get current timezone", err)
-							}
-							d.SetUserProperty("org.zsys:creation.test", strconv.FormatInt(s.CreationDate.In(location).Unix(), 10))
-
-							if s.LastBootedKernel != "" {
-								d.SetUserProperty("org.zsys:last-booted-kernel", s.LastBootedKernel)
-							}
-						}()
-					}
-
-					if shouldMount {
-						replaceContent(fdevice.T, dataset.Content, datasetPath)
-						completeSystemWithFstab(fdevice.T, testName, path, dataset.Mountpoint, datasetPath, dataset.ZsysBootfs, dataset.LastUsed, dataset.Fstab)
-					}
+					pool.Export(true, "export temporary pool")
 				}()
+
+				for _, dataset := range device.ZFS.Datasets {
+					func() {
+						datasetName := poolName + "/" + dataset.Name
+						if dataset.IsCurrentSystemRoot {
+							systemRootDataset = datasetName
+						}
+						datasetPath := ""
+						shouldMount := false
+						props := make(map[zfs.Prop]zfs.Property)
+						if dataset.Mountpoint != "" {
+							props[zfs.DatasetPropMountpoint] = zfs.Property{Value: dataset.Mountpoint}
+						}
+						if dataset.CanMount != "" {
+							props[zfs.DatasetPropCanmount] = zfs.Property{Value: dataset.CanMount}
+							if dataset.CanMount == "noauto" || dataset.CanMount == "on" {
+								shouldMount = true
+							}
+						}
+						d, err := zfs.DatasetCreate(datasetName, zfs.DatasetTypeFilesystem, props)
+						if err != nil {
+							fdevice.Fatalf("couldn't create dataset %q: %v", datasetName, err)
+						}
+						defer d.Close()
+						if dataset.ZsysBootfs {
+							d.SetUserProperty("org.zsys:bootfs", "yes")
+							if !dataset.LastUsed.IsZero() {
+								d.SetUserProperty("org.zsys:last-used", strconv.FormatInt(dataset.LastUsed.Unix(), 10))
+							}
+						}
+						if dataset.LastBootedKernel != "" {
+							d.SetUserProperty("org.zsys:last-booted-kernel", dataset.LastBootedKernel)
+						}
+						if shouldMount {
+							// get potentially inherited mountpoint path
+							mountProp, err := d.GetProperty(zfs.DatasetPropMountpoint)
+							if err != nil {
+								fdevice.Fatalf("couldn't get mount point for %q: %v", datasetName, err)
+							}
+							datasetPath = mountProp.Value
+							if datasetPath != "legacy" {
+								if err := d.Mount("", 0); err != nil {
+									fdevice.Fatalf("couldn't mount dataset: %q: %v", datasetName, err)
+								}
+								// Mount manually datasetPath if set to legacy to "/" (deviceMountPath)
+							} else {
+								datasetPath = deviceMountPath
+								if err := syscall.Mount(datasetName, datasetPath, "zfs", 0, ""); err != nil {
+									fdevice.Fatalf("couldn't manually mount dataset: %q: %v", datasetName, err)
+								}
+							}
+
+							defer os.RemoveAll(datasetPath)
+							defer d.UnmountAll(0)
+						}
+
+						for _, s := range dataset.Snapshots {
+							func() {
+								replaceContent(fdevice.T, s.Content, datasetPath)
+								completeSystemWithFstab(fdevice.T, testName, path, dataset.Mountpoint, datasetPath, false, time.Time{}, s.Fstab)
+								props := make(map[zfs.Prop]zfs.Property)
+								d, err := zfs.DatasetSnapshot(datasetName+"@"+s.Name, false, props)
+								if err != nil {
+									fmt.Fprintf(os.Stderr, "Couldn't create snapshot %q: %v\n", datasetName+"@"+s.Name, err)
+									os.Exit(1)
+								}
+								defer d.Close()
+
+								// Convert time in current timezone for mock
+								location, err := time.LoadLocation("Local")
+								if err != nil {
+									fdevice.Fatal("couldn't get current timezone", err)
+								}
+								d.SetUserProperty("org.zsys:creation.test", strconv.FormatInt(s.CreationDate.In(location).Unix(), 10))
+
+								if s.LastBootedKernel != "" {
+									d.SetUserProperty("org.zsys:last-booted-kernel", s.LastBootedKernel)
+								}
+							}()
+						}
+
+						if shouldMount {
+							replaceContent(fdevice.T, dataset.Content, datasetPath)
+							completeSystemWithFstab(fdevice.T, testName, path, dataset.Mountpoint, datasetPath, dataset.ZsysBootfs, dataset.LastUsed, dataset.Fstab)
+						}
+					}()
+				}
+
+			case "ext4":
+				func() {
+					ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+					defer cancel()
+					cmd := exec.CommandContext(ctx, "mkfs.ext4", "-q", "-F", p)
+					if err := cmd.Run(); err != nil {
+						fdevice.Fatalf("couldn't format %q to ext4: %v", p, err)
+					}
+
+					cmd = exec.CommandContext(ctx, "mount", "-t", "ext4", p, deviceMountPath)
+					if err := cmd.Run(); err != nil {
+						fdevice.Fatalf("couldn't mount ext4 partition: %v", err)
+					}
+					defer syscall.Unmount(deviceMountPath, 0)
+
+					replaceContent(fdevice.T, device.Content, deviceMountPath)
+				}()
+
+			case "":
+				// do nothing for "no pool, no partition" (empty disk)
+
+			default:
+				fdevice.Fatalf("unknown type: %s", device.Type)
 			}
-
-		case "ext4":
-			func() {
-				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-				defer cancel()
-				cmd := exec.CommandContext(ctx, "mkfs.ext4", "-q", "-F", p)
-				if err := cmd.Run(); err != nil {
-					fdevice.Fatalf("couldn't format %q to ext4: %v", p, err)
-				}
-
-				cmd = exec.CommandContext(ctx, "mount", "-t", "ext4", p, deviceMountPath)
-				if err := cmd.Run(); err != nil {
-					fdevice.Fatalf("couldn't mount ext4 partition: %v", err)
-				}
-				defer syscall.Unmount(deviceMountPath, 0)
-
-				replaceContent(fdevice.T, device.Content, deviceMountPath)
-			}()
-
-		case "":
-			// do nothing for "no pool, no partition" (empty disk)
-
-		default:
-			fdevice.Fatalf("unknown type: %s", device.Type)
-		}
+		}()
 	}
 
 	return systemRootDataset
